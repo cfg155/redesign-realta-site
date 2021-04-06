5bc89efad168dd2bffdbacf4c41db9cd81eb74a1
Louis Leonardo <67050078+cfg155@users.noreply.github.com>
2021-04-06 16:41:52 +0700

    init company page

---
+++ READ.md
@@ -0,0 +1,14 @@
+# Revisi 1 (Homepage)
+
+1. lbih di masukin lagi dan gambar nya rada ke kanan
+2. logo nya kecilin dikit
+3. lingkaran kotak us kecilin
+4. kasi strip 3 untuk navbar
+5. tinggi nya 0.5 lebar 2.5 untuk contact us
+6. sleekr.co untuk client
+7. border-radius kurangin
+8. accorodion kurangin tingginya
+9. button play animasiin
+10. section hp nya transisi
+11. total emp dari web siap+p
+12. footer responsive
--- appstarter/app/Views/company.php
+++ appstarter/app/Views/company.php
@@ -5,11 +5,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Company</title>
+    <link rel="stylesheet" href="css/company.css">
 </head>
 
 <body>
+    <!-- Include Navbar -->
+    <?= $this->include('layout/component/navbar'); ?>
 
+    <div class="container">
+
+    </div>
+
+    <!-- Include Footer -->
+    <?= $this->include('layout/component/footer'); ?>
 </body>
 
 </html>
