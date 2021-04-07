9626a3bcb90fd4998694b38c49638ef7d7226371
Louis Leonardo <67050078+cfg155@users.noreply.github.com>
2021-04-07 15:02:51 +0700

    convert implement bootstrap

--- appstarter/app/Views/layout/component/navbar.php
+++ appstarter/app/Views/layout/component/navbar.php
@@ -1,20 +1,28 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="css/navbar.css">
 <nav>
     <!-- This is Navbar area -->
-    <div class="nav">
-        <div class="nav--logo">
-            <img src="assets/LOGO-Realta-with-since-1983.png">
+    <div class="nav py-3 w-75 m-auto">
+        <div class="container">
+            <div class="d-flex align-items-center justify-content-between">
+                <img src="assets/LOGO-Realta-with-since-1983.png" class="nav--logo">
+                <div class="nav--links">
+                    <a href="<?php echo base_url('/solution') ?>" class="current text-decoration-none me-3 text-dark">SOLUTIONS</a>
+                    <a href="#" class="text-decoration-none me-3 text-dark">COMPANY</a>
+                    <a href="#" class="text-decoration-none me-3 text-dark">SUPPORT</a>
+                    <a href="#" class="text-decoration-none me-3 text-dark">RESOURCES</a>
+                    <a href="#" class="contact-us py-2 px-3 text-decoration-none text-light rounded-pill">CONTACT US</a>
+                </div>
+            </div>
+
+            <label for="switch" class="nav--switch d-flex justify-content-between ms-auto my-2">
+                <div class="ina-lang active w-100 py-1 d-flex align-items-center justify-content-center">INA</div>
+                <div class="en-lang py-1 w-100 d-flex align-items-center justify-content-center">ENG</div>
+            </label>
         </div>
-        <div class="nav--links">
-            <a href="#" class="current">SOLUTIONS</a>
-            <a href="<?php echo base_url('/solution') ?>">COMPANY</a>
-            <a href="#">SUPPORT</a>
-            <a href="#">RESOURCES</a>
-            <a href="#" class="contact-us">CONTACT US</a>
-        </div>
+
+
     </div>
-    <label for="switch" class="nav--switch">
-        <div class="ina-lang">INA</div>
-        <div class="en-lang">ENG</div>
-    </label>
+
 </nav>
