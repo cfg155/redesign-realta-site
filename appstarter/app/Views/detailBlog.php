fb238ceca61d79ec746987676c9f3cc9ade3e8f0
Louis Leonardo <67050078+cfg155@users.noreply.github.com>
2021-04-08 13:53:55 +0700

    implement detail blog side panel

--- appstarter/app/Views/detailBlog.php
+++ appstarter/app/Views/detailBlog.php
@@ -12,6 +12,51 @@
 <body>
     <!-- Include Navbar -->
     <?= $this->include('layout/component/navbar'); ?>
+
+    <div class="container-fluid">
+        <div class="container w-75 m-auto">
+            <div class="content">
+                <div class="content__container row">
+                    <div class="col-md-8">
+
+                    </div>
+                    <!-- Side Panel -->
+                    <div class="col-md-4">
+                        <!-- Popular Posts -->
+                        <div class="side-panel p-3 mb-5">
+                            <h4>Populer Post</h4>
+                            <div class="post-item d-flex align-items-center my-3">
+                                <img src="../../assets/Rectangle 85.png">
+                                <span class="px-2">Lorem, ipsum.</span>
+                            </div>
+                            <div class="post-item d-flex align-items-center my-3">
+                                <img src="../../assets/Rectangle 85.png">
+                                <span class="px-2">Lorem, ipsum.</span>
+                            </div>
+                            <div class="post-item d-flex align-items-center my-3">
+                                <img src="../../assets/Rectangle 85.png">
+                                <span class="px-2">Lorem, ipsum.</span>
+                            </div>
+                        </div>
+                        <!-- Category -->
+                        <div class="side-panel p-3 mb-5">
+                            <h4>Category</h4>
+                            <div class="category-item d-flex align-items-center my-3">
+                                <span class="px-2">Lorem, ipsum.</span>
+                            </div>
+                            <div class="category-item d-flex align-items-center my-3">
+                                <span class="px-2">Lorem, ipsum.</span>
+                            </div>
+                            <div class="category-item d-flex align-items-center my-3">
+                                <span class="px-2">Lorem, ipsum.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 
 </html>
