6d2fa2c36fa6ddc922ac09c137895cf6749cc416
Louis Leonardo <67050078+cfg155@users.noreply.github.com>
2021-04-09 09:32:35 +0700

    store sql data for data dummy example detail blog

---
+++ blog.sql
@@ -0,0 +1,5 @@
+insert into blog (id, title, published_by, read_time, content) values (1, 'Dead Hate the Living!, The', 'Madalyn', 5, null);
+insert into blog (id, title, published_by, read_time, content) values (2, 'Prairie Home Companion, A', 'Lezley', 10, null);
+insert into blog (id, title, published_by, read_time, content) values (3, 'Madagascar: Escape 2 Africa', 'Parke', 2, null);
+insert into blog (id, title, published_by, read_time, content) values (4, 'I Am Sam', 'Nickey', 8, null);
+insert into blog (id, title, published_by, read_time, content) values (5, 'Girls, Les', 'Erinna', 2, null);
