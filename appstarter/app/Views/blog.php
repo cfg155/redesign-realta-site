be19f19dd095c61dd0fa61a0d549ca8c006d6589
Louis Leonardo <67050078+cfg155@users.noreply.github.com>
2021-04-09 10:23:28 +0700

    change content__container__top to content_container__highlight

--- appstarter/app/Views/blog.php
+++ appstarter/app/Views/blog.php
@@ -19,8 +19,8 @@
                 <h1 class="content__title py-3">Blog</h1>
                 <div class="content__container row py-5">
                     <!-- Main Content -->
-                    <div class="col-md-8">
-                        <div class="content__container__top d-flex align-items-center mb-5">
+                    <div class="col-lg-8 col-md-12">
+                        <div class="content__container__highlight d-flex align-items-center mb-5">
                             <div class="row">
                                 <div class="col-lg-6 col-md-12 bg"></div>
 
@@ -65,42 +65,7 @@
                                     <div class="c-card w-75 m-auto">
                                         <img src="assets/Cue Card - Bandung Golf Gathering 2021.png" class="w-100">
                                         <h4>Lorem Ipsum</h4>
-                                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rem dicta! Expedita, mollitia at odit dolorum temporibus delectus quo eum!</p>
-                                        <div class="footer d-flex justify-content-between">
-                                            <div class="date d-flex align-items-center">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="18.75" height="20" viewBox="0 0 18.75 20" class="me-2">
-                                                    <defs>
-                                                        <style>
-                                                            .a {
-                                                                fill: #cecece;
-                                                            }
-                                                        </style>
-                                                    </defs>
-                                                    <path class="a" d="M8.821,9.428h2.5v2.5h-2.5Zm3.75,0h2.5v2.5h-2.5Zm3.75,0h2.5v2.5h-2.5Zm-11.25,7.5h2.5v2.5h-2.5Zm3.75,0h2.5v2.5h-2.5Zm3.75,0h2.5v2.5h-2.5Zm-3.75-3.75h2.5v2.5h-2.5Zm3.75,0h2.5v2.5h-2.5Zm3.75,0h2.5v2.5h-2.5Zm-11.25,0h2.5v2.5h-2.5Zm13.75-11.25v1.25h-2.5V1.928H7.571v1.25h-2.5V1.928h-2.5v20h18.75v-20h-2.5Zm1.25,18.75H3.821V6.928h16.25Z" transform="translate(-2.571 -1.928)" />
-                                                </svg>
-                                                <span>24 March 2021</span>
-                                            </div>
-                                            <div class="read-time d-flex align-items-center">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" class="me-2">
-                                                    <defs>
-                                                        <style>
-                                                            .a {
-                                                                fill: #cecece;
-                                                            }
-                                                        </style>
-                                                    </defs>
-                                                    <path class="a" d="M4.488,13.846a9.805,9.805,0,0,1,1.35-5.027,9.992,9.992,0,0,1,3.65-3.65,9.981,9.981,0,0,1,8.89-.551A10.09,10.09,0,0,1,21.566,6.75a9.792,9.792,0,0,1,2.131,3.2,9.931,9.931,0,0,1-5.32,13.091,9.967,9.967,0,0,1-7.78,0A9.841,9.841,0,0,1,5.27,17.727,9.624,9.624,0,0,1,4.488,13.846Zm2.2,0a7.548,7.548,0,0,0,2.3,5.488,7.5,7.5,0,0,0,5.506,2.3,7.55,7.55,0,0,0,3.9-1.048,7.879,7.879,0,0,0,2.851-2.842,7.736,7.736,0,0,0,0-7.806A7.835,7.835,0,0,0,18.4,7.087a7.776,7.776,0,0,0-7.8,0,8,8,0,0,0-2.86,2.851A7.52,7.52,0,0,0,6.69,13.846Zm7.034,0V7.931a.737.737,0,1,1,1.474,0v5.462l3.188,1.856a.725.725,0,0,1,.337.453.667.667,0,0,1-.08.551.692.692,0,0,1-.639.373.618.618,0,0,1-.373-.107l-3.393-1.98a.731.731,0,0,1-.373-.258A.675.675,0,0,1,13.724,13.846Z" transform="translate(-4.488 -3.828)" />
-                                                </svg>
-                                                <span>2 Min Read</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 my-4 ">
-                                    <div class="c-card w-75 m-auto">
-                                        <img src="assets/Cue Card - Bandung Golf Gathering 2021.png" class="w-100">
-                                        <h4>Lorem Ipsum</h4>
-                                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rem dicta! Expedita, mollitia at odit dolorum temporibus delectus quo eum!</p>
+                                        <p class="body-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rem dicta! Expedita, mollitia at odit dolorum temporibus delectus quo eum!</p>
                                         <div class="footer d-flex justify-content-between">
                                             <div class="date d-flex align-items-center">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="18.75" height="20" viewBox="0 0 18.75 20" class="me-2">
@@ -135,7 +100,42 @@
                                     <div class="c-card w-75 m-auto">
                                         <img src="assets/Cue Card - Bandung Golf Gathering 2021.png" class="w-100">
                                         <h4>Lorem Ipsum</h4>
-                                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rem dicta! Expedita, mollitia at odit dolorum temporibus delectus quo eum!</p>
+                                        <p class="body-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rem dicta! Expedita, mollitia at odit dolorum temporibus delectus quo eum!</p>
+                                        <div class="footer d-flex justify-content-between">
+                                            <div class="date d-flex align-items-center">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="18.75" height="20" viewBox="0 0 18.75 20" class="me-2">
+                                                    <defs>
+                                                        <style>
+                                                            .a {
+                                                                fill: #cecece;
+                                                            }
+                                                        </style>
+                                                    </defs>
+                                                    <path class="a" d="M8.821,9.428h2.5v2.5h-2.5Zm3.75,0h2.5v2.5h-2.5Zm3.75,0h2.5v2.5h-2.5Zm-11.25,7.5h2.5v2.5h-2.5Zm3.75,0h2.5v2.5h-2.5Zm3.75,0h2.5v2.5h-2.5Zm-3.75-3.75h2.5v2.5h-2.5Zm3.75,0h2.5v2.5h-2.5Zm3.75,0h2.5v2.5h-2.5Zm-11.25,0h2.5v2.5h-2.5Zm13.75-11.25v1.25h-2.5V1.928H7.571v1.25h-2.5V1.928h-2.5v20h18.75v-20h-2.5Zm1.25,18.75H3.821V6.928h16.25Z" transform="translate(-2.571 -1.928)" />
+                                                </svg>
+                                                <span>24 March 2021</span>
+                                            </div>
+                                            <div class="read-time d-flex align-items-center">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" class="me-2">
+                                                    <defs>
+                                                        <style>
+                                                            .a {
+                                                                fill: #cecece;
+                                                            }
+                                                        </style>
+                                                    </defs>
+                                                    <path class="a" d="M4.488,13.846a9.805,9.805,0,0,1,1.35-5.027,9.992,9.992,0,0,1,3.65-3.65,9.981,9.981,0,0,1,8.89-.551A10.09,10.09,0,0,1,21.566,6.75a9.792,9.792,0,0,1,2.131,3.2,9.931,9.931,0,0,1-5.32,13.091,9.967,9.967,0,0,1-7.78,0A9.841,9.841,0,0,1,5.27,17.727,9.624,9.624,0,0,1,4.488,13.846Zm2.2,0a7.548,7.548,0,0,0,2.3,5.488,7.5,7.5,0,0,0,5.506,2.3,7.55,7.55,0,0,0,3.9-1.048,7.879,7.879,0,0,0,2.851-2.842,7.736,7.736,0,0,0,0-7.806A7.835,7.835,0,0,0,18.4,7.087a7.776,7.776,0,0,0-7.8,0,8,8,0,0,0-2.86,2.851A7.52,7.52,0,0,0,6.69,13.846Zm7.034,0V7.931a.737.737,0,1,1,1.474,0v5.462l3.188,1.856a.725.725,0,0,1,.337.453.667.667,0,0,1-.08.551.692.692,0,0,1-.639.373.618.618,0,0,1-.373-.107l-3.393-1.98a.731.731,0,0,1-.373-.258A.675.675,0,0,1,13.724,13.846Z" transform="translate(-4.488 -3.828)" />
+                                                </svg>
+                                                <span>2 Min Read</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 my-4">
+                                    <div class="c-card w-75 m-auto">
+                                        <img src="assets/Cue Card - Bandung Golf Gathering 2021.png" class="w-100">
+                                        <h4>Lorem Ipsum</h4>
+                                        <p class="body-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rem dicta! Expedita, mollitia at odit dolorum temporibus delectus quo eum!</p>
                                         <div class="footer d-flex justify-content-between">
                                             <div class="date d-flex align-items-center">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="18.75" height="20" viewBox="0 0 18.75 20" class="me-2">
@@ -171,9 +171,9 @@
                         </div>
                     </div>
                     <!-- Side Panel -->
-                    <div class="col-md-4">
+                    <div class="col-lg-4 col-md-12">
                         <!-- Popular Posts -->
-                        <div class="side-panel p-3 mb-5">
+                        <div class=" side-panel p-3 mb-5">
                             <h4>Populer Post</h4>
                             <div class="post-item d-flex align-items-center my-3">
                                 <img src="assets/Rectangle 85.png">
