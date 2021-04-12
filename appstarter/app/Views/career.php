c48ab32fe6e071882987c2a991afa3ad6cccb873
Louis Leonardo <67050078+cfg155@users.noreply.github.com>
2021-04-12 14:52:55 +0700

    career page is completed

--- appstarter/app/Views/career.php
+++ appstarter/app/Views/career.php
@@ -6,11 +6,100 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Career</title>
+    <link rel="stylesheet" href="css/career.css">
+
 </head>
 
 <body>
     <!-- Include Navbar -->
     <?= $this->include('layout/component/navbar'); ?>
+
+    <div class="container-fluid">
+        <div class="container w-75 m-auto">
+            <h2>Career <br> Realta Chakradarma</h2>
+            <div class="row career--header">
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <span class="input-group-text" id="basic-addon1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
+                            </svg>
+                        </span>
+                        <input type="text" class="form-control" placeholder="Search Job">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                            </svg>
+                        </span>
+                        <select name="" id="" class="form-control">
+                            <option value="">Jakarta</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+            <div class="career pt-3 pb-5">
+                <div class="career--item w-75 p-4 mb-3 border border-dark">
+                    <div class="header row">
+                        <h5 class="col-md-8">Frontend Developer</h5>
+                        <div class="location col-md-4 d-flex justify-content-md-end align-items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                            </svg>
+                            <span>Semarang</span>
+                        </div>
+                    </div>
+                    <p class="py-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus id fuga maxime exercitationem libero a nisi corporis quaerat delectus! Quod ut fugit, itaque possimus reiciendis praesentium. Accusantium maiores amet vitae?</p>
+                    <div class="footer row">
+                        <span class="col-md-8 mb-3">Batasan Waktu : 17 April 2021</span>
+                        <div class="btn-apply col-md-4 d-flex justify-content-md-end">
+                            <a href="#" class="btn btn-apply rounded-pill text-dark text-decoration-none text-white px-5">APPLY</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="career--item w-75 p-4 mb-3 border border-dark">
+                    <div class="header row">
+                        <h5 class="col-md-8">Frontend Developer</h5>
+                        <div class="location col-md-4 d-flex justify-content-md-end align-items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                            </svg>
+                            <span>Semarang</span>
+                        </div>
+                    </div>
+                    <p class="py-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus id fuga maxime exercitationem libero a nisi corporis quaerat delectus! Quod ut fugit, itaque possimus reiciendis praesentium. Accusantium maiores amet vitae?</p>
+                    <div class="footer row">
+                        <span class="col-md-8 mb-3">Batasan Waktu : 17 April 2021</span>
+                        <div class="btn-apply col-md-4 d-flex justify-content-md-end">
+                            <a href="#" class="btn btn-apply rounded-pill text-dark text-decoration-none text-white px-5">APPLY</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="career--item w-75 p-4 mb-3 border border-dark">
+                    <div class="header row">
+                        <h5 class="col-md-8">Frontend Developer</h5>
+                        <div class="location col-md-4 d-flex justify-content-md-end align-items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                            </svg>
+                            <span>Semarang</span>
+                        </div>
+                    </div>
+                    <p class="py-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus id fuga maxime exercitationem libero a nisi corporis quaerat delectus! Quod ut fugit, itaque possimus reiciendis praesentium. Accusantium maiores amet vitae?</p>
+                    <div class="footer row">
+                        <span class="col-md-8 mb-3">Batasan Waktu : 17 April 2021</span>
+                        <div class="btn-apply col-md-4 d-flex justify-content-md-end">
+                            <a href="#" class="btn btn-apply rounded-pill text-dark text-decoration-none text-white px-5">APPLY</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <!-- Include Footer -->
     <?= $this->include('layout/component/footer'); ?>
