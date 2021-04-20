92f7d96f78cf78ae6bd9e352c7afbd86f6e05f2d
Louis Leonardo <67050078+cfg155@users.noreply.github.com>
2021-04-20 16:36:25 +0700

    change width unit and responsive mobile

--- appstarter/app/Views/layout/component/navbar.php
+++ appstarter/app/Views/layout/component/navbar.php
@@ -3,7 +3,7 @@
 
 <nav>
     <!-- This is Navbar area -->
-    <div class="nav  m-auto">
+    <div class="nav">
         <div class="container w-100 position-relative">
             <div class="d-flex py-3 align-items-center justify-content-between">
                 <a href="<?php echo base_url('/') ?>"><img src="assets/LOGO-Realta-with-since-1983.png" class="nav--logo"></a>
