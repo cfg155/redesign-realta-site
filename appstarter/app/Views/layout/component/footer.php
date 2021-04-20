016a22ad84c8d5fbaa84da6757f96a916066f3bc
Louis Leonardo <67050078+cfg155@users.noreply.github.com>
2021-04-20 16:21:39 +0700

    fix footer margin by removing class w-75 and added style instead and responsive style

--- appstarter/app/Views/layout/component/footer.php
+++ appstarter/app/Views/layout/component/footer.php
@@ -2,43 +2,39 @@
 
 <footer>
     <div class="container-fluid w-100 pb-5">
-        <div class="w-75 m-auto">
-            <div class="footer__wrapper p-4 overflow-hidden">
-                <div class="links">
-                    <a href="#" class="text-decoration-none text-dark me-4">Solutions</a>
-                    <a href="#" class="text-decoration-none text-dark me-4">Company</a>
-                    <a href="#" class="text-decoration-none text-dark me-4">Support</a>
-                    <a href="#" class="text-decoration-none text-dark me-4">Resources</a>
-                </div>
-                <hr class="mb-4">
-                <div class="row d-flex align-items-center">
-                    <div class="col-lg-4">
-                        <div class="social-media d-flex align-items-baseline">
-                            <a href="" class="icon me-3">
-                                <img src="assets/Group 133.png" alt="">
-                            </a>
-                            <a href="" class="icon me-3">
-                                <img src="assets/Group 134.png" alt="">
-                            </a>
-                            <a href="" class="icon me-3">
-                                <img src="assets/Group 135.png" alt="">
-                            </a>
-                            <a href="" class="icon me-3">
-                                <img src="assets/Group 136.png" alt="">
-                            </a>
-                            <a href="" class="icon me-3">
-                                <img src="assets/Group 154.png" alt="">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-lg-8 py-3 text-lg-end">
-                        &copy;2021 PT Realta Chakradarma. All Rights Reserved
-                    </div>
-
-                </div>
+        <div class="footer__wrapper p-4 overflow-hidden">
+            <div class="links">
+                <a href="#" class="text-decoration-none text-dark me-4">Solutions</a>
+                <a href="#" class="text-decoration-none text-dark me-4">Company</a>
+                <a href="#" class="text-decoration-none text-dark me-4">Support</a>
+                <a href="#" class="text-decoration-none text-dark me-4">Resources</a>
             </div>
+            <hr class="mb-4">
+            <div class="row d-flex align-items-center">
+                <div class="col-lg-4">
+                    <div class="social-media d-flex align-items-baseline">
+                        <a href="" class="icon me-3">
+                            <img src="assets/Group 133.png" alt="">
+                        </a>
+                        <a href="" class="icon me-3">
+                            <img src="assets/Group 134.png" alt="">
+                        </a>
+                        <a href="" class="icon me-3">
+                            <img src="assets/Group 135.png" alt="">
+                        </a>
+                        <a href="" class="icon me-3">
+                            <img src="assets/Group 136.png" alt="">
+                        </a>
+                        <a href="" class="icon me-3">
+                            <img src="assets/Group 154.png" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-8 py-3 text-lg-end">
+                    &copy;2021 PT Realta Chakradarma. All Rights Reserved
+                </div>
 
-
+            </div>
         </div>
     </div>
 
